5311f5c9a7673e331ee0d8d00861062e26bc9d11
MaryVH08 <marycarmen.vives@aneiap.co>
2024-12-24 04:03:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -371,8 +371,9 @@ def generate_report(pdf_path, position, candidate_name):
     pdf.cell(200, 10, txt=f"Análisis por Indicadores:", ln=True)
     pdf.set_font("Arial", style="", size=12)
     for indicator, percentage in indicator_results.items():
-        pdf.cell(0, 10, f"- {indicator}: {percentage:.2f}%", ln=True)
-    low_performance_indicators = {k: v for k, v in indicator_results.items() if v < 50.0}
+        st.write(f"- {indicator}: {percentage:.2f}%")
+    lowest_indicator = min(indicator_results, key=indicator_results.get)
+    st.write(f"Indicador con menor presencia: {lowest_indicator} ({indicator_results[lowest_indicator]:.2f}%)")
     if low_performance_indicators:
         pdf.set_font("Arial", style="B", size=12)
         pdf.cell(0, 10, "Consejos para Mejorar:", ln=True)
