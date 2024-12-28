92726f8aec82ee8e23bce701c10a065dec3b7394
MaryVH08 <marycarmen.vives@aneiap.co>
2024-12-28 03:06:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -239,7 +239,8 @@ def extract_experience_section_with_ocr(pdf_path):
         "trabajo capitular",
         "trabajo nacional",
         "actualización profesional",
-        "nacional 2021-2023"
+        "nacional 2021-2023",
+        "nacional 20212023"
     ]
     
     experience_lines = experience_text.split("\n")
