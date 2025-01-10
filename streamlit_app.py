7355bd1cc85729dd61ab8d6d4d82315cd5f6149c
MaryVH08 <marycarmen.vives@aneiap.co>
2025-01-09 19:17:13 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import fitz  # PyMuPDF para trabajar con PDFs
 from reportlab.pdfgen import canvas
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.enums import TA_JUSTIFY
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.utils import ImageReader
 from reportlab.pdfbase.ttfonts import TTFont
