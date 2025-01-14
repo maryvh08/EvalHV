d9a5b37e852058c0cf89ee4c1b645e6faeae851f
MaryVH08 <marycarmen.vives@aneiap.co>
2025-01-14 17:32:52 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -418,6 +418,20 @@ def generate_report_with_background(pdf_path, position, candidate_name,backgroun
     # Insertar un salto de página
     elements.append(PageBreak())
 
+    #Insertar gráficas de indicadores
+    chart_rows = []
+    chart_labels = []
+
+    for indicator, data in indicator_results.items():
+        percentage = data.get("percentage", 0) if isinstance(data, dict) else data
+        if isinstance(percentage, (int, float)):  # Validar que sea un número
+            chart_buffer = generate_donut_chart_for_report(percentage)
+            chart_image = RLImage(chart_buffer, 2 * inch, 2 * inch)  # Crear imagen de gráfico
+            chart_rows.append(chart_image)  # Agregar gráfico a la fila
+            chart_labels.append(Paragraph(indicator, styles['CenturyGothic']))  # Agregar nombre del indicador
+        else:
+            st.warning(f"El porcentaje para {indicator} no es válido: {percentage}")
+
     # Organizar gráficos y nombres en filas
     combined_rows = []
     for i in range(0, len(chart_rows), 3):
