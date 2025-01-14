91a4192d5b2df3dbb79f039baef0c75787b8ad7a
MaryVH08 <marycarmen.vives@aneiap.co>
2025-01-14 17:43:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -421,6 +421,10 @@ def generate_report_with_background(pdf_path, position, candidate_name,backgroun
     #Insertar gráficas de indicadores
     chart_rows = []
     for indicator, data in indicator_results.items():
+        relevant_lines = sum(
+            any(keyword.lower() in line.lower() for keyword in keywords) for line in lines
+        )
+        percentage = (relevant_lines / total_lines) * 100 if total_lines > 0 else 0
         if isinstance(percentage, (int, float)):  # Validar que sea un número
             chart_buffer = generate_donut_chart_for_report(percentage, color=green)
             chart_image = RLImage(chart_buffer, 2 * inch, 2 * inch)  # Usar RLImage para evitar conflictos
