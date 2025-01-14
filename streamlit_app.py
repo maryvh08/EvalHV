ddd19a103de02722c0f51df85615a3193cb3b580
MaryVH08 <marycarmen.vives@aneiap.co>
2025-01-14 16:20:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -423,7 +423,9 @@ def generate_report_with_background(pdf_path, position, candidate_name,backgroun
     chart_labels = []
 
     for indicator, data in indicator_results.items():
-        relevant_lines = result["relevant_lines"]
+        relevant_lines = sum(
+            any(keyword.lower() in line.lower() for keyword in keywords) for line in lines
+        )
         percentage = (relevant_lines / total_lines) * 100 if total_lines > 0 else 0
         if isinstance(percentage, (int, float)):  # Validar que sea un número
             chart_buffer = generate_donut_chart_for_report(percentage)
