ed3e05d51442b07627dc02ea3ccd5e8d35446753
MaryVH08 <marycarmen.vives@aneiap.co>
2025-01-23 23:38:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -498,7 +498,7 @@ def generate_report_with_background(pdf_path, position, candidate_name,backgroun
     org_table_data = [["Ítem", "Funciones del Cargo (%)", "Perfil del Cargo (%)"]]
     
     # Agregar datos de line_results a la tabla
-    for line, func_match, profile_match in line_results:
+    for line, func_match, profile_match in org_line_results:
         org_table_data.append([Paragraph(line, styles['CenturyGothic']), f"{org_func_match:.2f}%", f"{org_profile_match:.2f}%"])
 
     #Agregar resultados parciales
