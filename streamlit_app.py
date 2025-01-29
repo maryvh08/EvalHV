c366d284a6c94d97588c9d10a8761640479ee616
MaryVH08 <marycarmen.vives@aneiap.co>
2025-01-29 01:24:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2199,8 +2199,8 @@ def analyze_and_generate_descriptive_report_with_background(pdf_path, position, 
     
     # **Agregar los puntajes combinados a la tabla**
     presentation_table_data.append(["Ortografía", f"{round_spelling_score:.2f}"])
-    presentation_table_data.append(["Capitalización y Puntuación", f"{round_capitalization_score:.2f}"])
-    presentation_table_data.append(["Coherencia y Repetición", f"{round_coherence_score:.2f}"])
+    presentation_table_data.append(["Gramática", f"{round_capitalization_score:.2f}"])
+    presentation_table_data.append(["Coherencia", f"{round_coherence_score:.2f}"])
     presentation_table_data.append(["Puntaje Total", f"{round_overall_score:.2f}"])
     
     # **Crear la tabla con ancho ajustado para las columnas**
