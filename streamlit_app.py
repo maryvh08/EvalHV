5a82b9df43d3d66f4315da99e8738f5cf41c3f90
MaryVH08 <marycarmen.vives@aneiap.co>
2025-03-25 13:24:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -988,43 +988,43 @@ def generate_report_with_background(pdf_path, position, candidate_name,backgroun
             # Calcular puntaje final de fluidez
             fluency_score = (punctuation_score + connector_score + variance_penalty) / 3
     return round(fluency_score, 2)  # Escalar a un rango de 0 a 100 y redondear
-      
-  # Calcular métricas individuales
-  repetition_score, repeated_words = calculate_word_repetition(pres_cleaned_lines)
-  fluency_score = calculate_sentence_fluency(pres_cleaned_lines)
-  
-  # Asegurar que repetition_score y fluency_score están entre 0 y 1 antes de la conversión
-  normalized_repetition_score = min(1, max(0, repetition_score))
-  normalized_fluency_score = min(1, max(0, fluency_score))
-  
-  # Calcular coherencia asegurando que el resultado final no pase de 5
-  coherence_score = round(min(5, (normalized_repetition_score + normalized_fluency_score) * 2.5), 2)
-  
-  # Puntaje general ponderado
-  overall_score = round((spelling_score  + coherence_score + grammar_score) / 3, 2)
-  
-  # Calculo puntajes parciales
-  parcial_exp_func_score = round((parcial_exp_func_match * 5) / 100, 2)
-  parcial_exp_profile_score = round((parcial_exp_profile_match * 5) / 100, 2)
-  parcial_org_func_score = round((parcial_org_func_match * 5) / 100, 2)
-  parcial_org_profile_score = round((parcial_org_profile_match * 5) / 100, 2)
-  parcial_att_func_score = round((parcial_att_func_match * 5) / 100, 2)
-  parcial_att_profile_score = round((parcial_att_profile_match * 5) / 100, 2)
-  profile_func_score= round((profile_func_match * 5) / 100, 2)
-  profile_profile_score= round((profile_profile_match * 5) / 100, 2)
-  
-  #Calcular resultados globales
-  global_func_match = (parcial_exp_func_match + parcial_att_func_match + parcial_org_func_match+ profile_func_match) / 4
-  global_profile_match = (parcial_exp_profile_match + parcial_att_profile_match + parcial_org_profile_match + profile_profile_match) / 4
-  func_score = round((global_func_match * 5) / 100, 2)
-  profile_score = round((global_profile_match * 5) / 100, 2)
-  
-  #Calculo puntajes totales
-  exp_score= (parcial_exp_func_score+ parcial_exp_profile_score)/2
-  org_score= (parcial_org_func_score+ parcial_org_profile_score)/2
-  att_score= (parcial_att_func_score+ parcial_att_profile_score)/2
-  prof_score= (profile_func_score+ profile_profile_score)/2
-  total_score= (overall_score+ exp_score+ org_score+ att_score+ profile_score)/5
+          
+    # Calcular métricas individuales
+    repetition_score, repeated_words = calculate_word_repetition(pres_cleaned_lines)
+    fluency_score = calculate_sentence_fluency(pres_cleaned_lines)
+    
+    # Asegurar que repetition_score y fluency_score están entre 0 y 1 antes de la conversión
+    normalized_repetition_score = min(1, max(0, repetition_score))
+    normalized_fluency_score = min(1, max(0, fluency_score))
+    
+    # Calcular coherencia asegurando que el resultado final no pase de 5
+    coherence_score = round(min(5, (normalized_repetition_score + normalized_fluency_score) * 2.5), 2)
+    
+    # Puntaje general ponderado
+    overall_score = round((spelling_score  + coherence_score + grammar_score) / 3, 2)
+    
+    # Calculo puntajes parciales
+    parcial_exp_func_score = round((parcial_exp_func_match * 5) / 100, 2)
+    parcial_exp_profile_score = round((parcial_exp_profile_match * 5) / 100, 2)
+    parcial_org_func_score = round((parcial_org_func_match * 5) / 100, 2)
+    parcial_org_profile_score = round((parcial_org_profile_match * 5) / 100, 2)
+    parcial_att_func_score = round((parcial_att_func_match * 5) / 100, 2)
+    parcial_att_profile_score = round((parcial_att_profile_match * 5) / 100, 2)
+    profile_func_score= round((profile_func_match * 5) / 100, 2)
+    profile_profile_score= round((profile_profile_match * 5) / 100, 2)
+    
+    #Calcular resultados globales
+    global_func_match = (parcial_exp_func_match + parcial_att_func_match + parcial_org_func_match+ profile_func_match) / 4
+    global_profile_match = (parcial_exp_profile_match + parcial_att_profile_match + parcial_org_profile_match + profile_profile_match) / 4
+    func_score = round((global_func_match * 5) / 100, 2)
+    profile_score = round((global_profile_match * 5) / 100, 2)
+    
+    #Calculo puntajes totales
+    exp_score= (parcial_exp_func_score+ parcial_exp_profile_score)/2
+    org_score= (parcial_org_func_score+ parcial_org_profile_score)/2
+    att_score= (parcial_att_func_score+ parcial_att_profile_score)/2
+    prof_score= (profile_func_score+ profile_profile_score)/2
+    total_score= (overall_score+ exp_score+ org_score+ att_score+ profile_score)/5
   
   # Registrar la fuente personalizada
   pdfmetrics.registerFont(TTFont('CenturyGothic', 'Century_Gothic.ttf'))
