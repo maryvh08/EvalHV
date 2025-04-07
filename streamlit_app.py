de37c149f1b443b38acfcc4eb06073bbdfc00aa1
MaryVH08 <marycarmen.vives@aneiap.co>
2025-04-06 20:04:09 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1579,11 +1579,11 @@ def generate_report_with_background(pdf_path, position, candidate_name,backgroun
     if low_performance_indicators:
       elements.append(Paragraph("<b>Consejos para Mejorar:</b>", styles['CenturyGothicBold']))
       for indicator, result in low_performance_indicators.items():
-        total_lines = len(line_results)
-        relevant_lines = sum(
-            any(keyword.lower() in line.lower() for keyword in keywords) for line in lines
-        )
-        percentage = (relevant_lines / total_lines) * 100
+          total_lines = len(line_results)
+            relevant_lines = sum(
+                any(keyword.lower() in line.lower() for keyword in keywords) for line in lines
+            )
+            percentage = (relevant_lines / total_lines) * 100
           elements.append(Paragraph(f" {indicator}: ({percentage:.2f}%)", styles['CenturyGothicBold']))
           elements.append(Spacer(1, 0.05 * inch))
           for tip in advice[position].get(indicator, []):
